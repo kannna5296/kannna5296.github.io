c148d4ffcba823e30d0341a48f8ec34e0c8c1505
kannna5296 <kannnahacks5296@gmail.com>
2022-02-23 16:01:55 +0900

    API叩く

---
+++ myranking/js/ranking.js
@@ -0,0 +1,14 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.spotify.com/v1/me/top/tracks', true);
+
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('Accept', 'application/json');
+//ここにキー入れる
+request.responseType = 'json';
+
+request.onload = function () {
+  var data = this.response;
+  console.log(data);
+};
+
+request.send();
