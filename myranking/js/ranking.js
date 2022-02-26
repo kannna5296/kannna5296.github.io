6e60b7243ed7e1ba26538755770a7ee2dc6e9ad4
kannna5296 <kannnahacks5296@gmail.com>
2022-02-26 18:40:07 +0900

    asyncawait

--- myranking/js/ranking.js
+++ myranking/js/ranking.js
@@ -1,14 +1,24 @@
 //トークン取得
-fetch('https://accounts.spotify.com/api/token', {
+var tokenAAA = 'token';
+
+async function getAccessToken() {
+
+const res =  await fetch('https://accounts.spotify.com/api/token', {
   mode: 'cors',
   method: 'POST',
   headers: {
-    'Authorization': 'Basic ',
+    'Authorization': 'Basic YzZmNjg0NjdjYWUwNDY2M2E1YWNiZmE2YmY4MDRjOWU6YmE0NDUwOGU0YTQzNDhiNzg3YjM2ZmFlODY4ZjE3Y2Q=',
     'Content-Type': 'application/x-www-form-urlencoded'
   },
-  body: 'grant_type=client_credentials'})
-  .then(response => {
-    if (response.ok) {
-      console.info(response.json());
-    }
-  });
+  body: 'grant_type=client_credentials'});
+
+resJson = await res.json();
+console.log(resJson.access_token);
+tokenAAA = resJson.access_token;
+
+}
+
+
+getAccessToken();
+
+console.log(tokenAAA);
