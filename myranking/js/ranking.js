d9c49f63049251a696ec1340c464aec7c43198c3
kannna5296 <kannnahacks5296@gmail.com>
2022-02-27 18:21:13 +0900

    レンダリング追加

--- myranking/js/ranking.js
+++ myranking/js/ranking.js
@@ -1,7 +1,9 @@
 //トークン取得
 //urlencoded処理
-var data;
-var details = {grant_type: "refresh_token", refresh_token: "認証トークン"};
+
+//毎回認証トークンを取るのアホすぎるのでどうにかしたい
+//認証に用いる値を隠したい
+var details = {grant_type: "refresh_token", refresh_token: "リフレッシュトークン"};
 var formBody = [];
 for (var property in details) {
   var encodedKey = encodeURIComponent(property);
@@ -9,13 +11,11 @@ for (var property in details) {
   formBody.push(encodedKey + "=" + encodedValue);
 }
 formBody = formBody.join("&");
-
-//ランキング取得
 fetch('https://accounts.spotify.com/api/token', {
   mode: 'cors',
   method: 'POST',
   headers: {
-    'Authorization': 'Basic base64したやつ',
+    'Authorization': 'Basic Basic64',
     'Content-Type': 'application/x-www-form-urlencoded'
   },
   body: formBody})
@@ -25,9 +25,8 @@ fetch('https://accounts.spotify.com/api/token', {
       console.log(tokens.access_token);
     })
   });
-
   function getRanking(accessToken){
-  fetch('https://api.spotify.com/v1/me/top/tracks?limit=30', {
+  fetch('https://api.spotify.com/v1/me/top/tracks?limit=50', {
   mode: 'cors',
   method: 'GET',
   headers: {
@@ -38,34 +37,53 @@ fetch('https://accounts.spotify.com/api/token', {
   .then((res) => {
     res.json().then(res => {
       console.log(res);
-      //レンダリング
       Rendering(res);
     })
   });
 }
-
+//ゴリ押しレンダリング フレームワーク化したい
 function Rendering(res){
   var rankingOb = document.getElementById('ranking');
   for (var item of res.items){
 
     var newElemPTitle = document.createElement("p");
     var textTitle = document.createTextNode(item.name);
+    newElemPTitle.classList.add('font-weight-bold');
     newElemPTitle.appendChild(textTitle);
 
     var newElemPArtist = document.createElement("p");
     var textArtist = document.createTextNode(item.artists[0].name);
     newElemPArtist.appendChild(textArtist);
 
-    var newElemDiv = document.createElement("div");
-    newElemDiv.appendChild(newElemPTitle);
-    newElemDiv.appendChild(newElemPArtist);
+    var newElemDivInfo = document.createElement("div");
+    newElemDivInfo.appendChild(newElemPTitle);
+    newElemDivInfo.appendChild(newElemPArtist);
+
+    var newElemImg = document.createElement("img");
+    newElemImg.src=item.album.images[2].url;
+
+    var newElemDivImg = document.createElement("div");
+    newElemDivImg.classList.add("pr-1");
+    newElemDivImg.appendChild(newElemImg);
+
+    var newElemDivFlex = document.createElement("div");
+    newElemDivFlex.classList.add("d-flex");
+    newElemDivFlex.classList.add("flex-row");
+    newElemDivFlex.appendChild(newElemDivImg);
+    newElemDivFlex.appendChild(newElemDivInfo);
 
     var newElemA = document.createElement("a");
     newElemA.href = item.external_urls.spotify;
-    newElemA.appendChild(newElemDiv);
+    newElemA.target = '_blank';
+    newElemA.rel = 'noopener noreferer';
+    newElemA.classList.add("flex");
+    newElemA.classList.add("flex-row");
+    newElemA.classList.add("float3");
+    newElemA.appendChild(newElemDivFlex);
 
     var newElemLi = document.createElement("li");
     newElemLi.appendChild(newElemA);
+
     rankingOb.appendChild(newElemLi);
   }
 }
